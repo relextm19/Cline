4cb6009b0b0e7aa0cabdb38b49b969ca1c0d3939
Mateusz kielb <relextm19@gmail.com>
2024-08-31 01:00:54 +0200

    misplaced comment fix

--- source_files/main.cpp
+++ source_files/main.cpp
@@ -3,14 +3,12 @@
 #include "../header_files/args.h"
 #include "../header_files/file_actions.h"
 
-
-
 int main(int argc, char* argv[]) {
     std::filesystem::path path = std::filesystem::current_path();
     std::vector<File_Data> file_datas;
     parse_args(argc, argv, &path);
     itterate_files(path, &file_datas);
     print_header();
-    // print_data(file_datas);
+    print_data(file_datas);
     print_footer();
 }
