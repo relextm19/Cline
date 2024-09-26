1e5415e10b902d5885d8071829c1287436e9f8e8
relextm19 <155774492+relextm19@users.noreply.github.com>
2024-09-26 19:44:40 +0200

    Update args.cpp

--- source_files/args.cpp
+++ source_files/args.cpp
@@ -1,10 +1,6 @@
 #include "../header_files/args.h"
 #include "../header_files/common.h"
 
-char argument_list[] = {
-    'p','h', 'r', 'e', 'd'
-};
-
 void parse_args(int argc, char* argv[], std::filesystem::path* path){
     if(path == nullptr) throw std::runtime_error("failed to initalize path");
     //we iterate over args
@@ -13,19 +9,9 @@ void parse_args(int argc, char* argv[], std::filesystem::path* path){
 
         //if we find a '-' we itterate through the argument list and see if we get a match
         for(int j = 0; j < sizeof(argument_list) / sizeof(argument_list[0]); j++){
-            if(argv[i][1] != argument_list[j]) continue;
-            int k = 0;
-            
             switch(argument_list[j]){
-                //if the arg is p we add a string(if it occurs) which follows the argument to the path 
-                case 'p':
-                    if(i + 1 > argc || argv[i+1][0] == '-') break;
-                    path->clear();
-                    *path = argv[i + 1];
-                    break;
-
+                //TODO
                 case 'h':
-                    std::cout << "Usage: " << argv[0] << " -p <path>" << std::endl;
                     break;
 
                 case 'r':
@@ -43,7 +29,6 @@ void parse_args(int argc, char* argv[], std::filesystem::path* path){
 
                     banned_directories.push_back(argv[i + 1]);
                     break;
-
 
                 default:
                     break;
